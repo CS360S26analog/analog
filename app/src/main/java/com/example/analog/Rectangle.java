8f7065a135d0932ea8fcb7c9b60f631cdc49582a
izzashahidd <27100406@lums.edu.pk>
2026-02-17 01:37:08 +0500

    Added Rectangle class extending Shape

---
+++ app/src/main/java/com/example/analog/Rectangle.java
@@ -0,0 +1,13 @@
+package com.example.analog;
+
+public class Rectangle extends Shape {
+
+    double width;
+    double height;
+
+    public Rectangle(double width, double height) {
+        super();
+        this.width = width;
+        this.height = height;
+    }
+}
